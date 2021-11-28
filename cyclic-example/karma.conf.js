c9c9b6cd3c852a1318217b3e6c42b132528ee009
Maor Assayag <maorassayag16@gmail.com>
2021-11-28 23:38:40 +0200

    .

--- cyclic-example/karma.conf.js
+++ cyclic-example/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/untitled'),
+      dir: require('path').join(__dirname, './coverage/chartjs-plugin-cyclic-axis-example'),
       subdir: '.',
       reporters: [
         { type: 'html' },
